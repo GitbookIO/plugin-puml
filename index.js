890095df7273ce958d6e796a8335a600f6379f39
Михайлов Антон <anion155@gmail.com>
2016-06-13 03:33:18 +0600

    Adding ability to provide source plantuml file through src attribute.

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 var plantumlEncoder = require('plantuml-encoder');
+var fs = require('fs');
 
 module.exports = {
     blocks: {
         plantuml: {
             process: function(block) {
-                var defaultFormat = this.generator == 'ebook'? 'png' : 'svg';
+                var source;
+                if (!!block.kwargs.src) {
+                    source = fs.readFileSync(block.kwargs.src);
+                } else {
+                    source = block.body;
+                }
+
+                var defaultFormat = this.output.name == 'ebook' ? 'png' : 'svg';
                 var format = block.kwargs.format || defaultFormat;
 
                 // Generate url
-                var encoded = plantumlEncoder.encode(block.body);
+                var encoded = plantumlEncoder.encode(source);
                 var href = 'http://www.plantuml.com/plantuml/' + format + '/' + encoded;
 
                 return '<img src="' + href + '" />';
