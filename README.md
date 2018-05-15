1ec774446be586a081aab70cf554392024650714
uwinkler <uli.winkler@gmail.com>
2018-05-15 09:22:20 +0200

    PUML_SERVER environemnt variable

--- README.md
+++ README.md
@@ -5,7 +5,6 @@ UML Diagrams rendering using PlantUML.
 [![Build Status](https://travis-ci.org/GitbookIO/plugin-puml.png?branch=master)](https://travis-ci.org/GitbookIO/plugin-puml)
 [![NPM version](https://badge.fury.io/js/gitbook-plugin-puml.svg)](http://badge.fury.io/js/gitbook-plugin-puml)
 
-
 Configure the plugin in your `book.json`:
 
 ```js
@@ -25,3 +24,16 @@ Bob->Alice : hello
 ```
 
 The plugin will replace the `{% plantuml %}` by SVG images (and PNG images for ebook output).
+
+# PUML Server
+
+To rendert UML diagrams the public PlantUML server http://www.plantuml.com is used.
+
+However, if you wish to run your own server - for example in a corporated environment settings - you may wish to run your own PlantUML server.
+To point the plugin to your server set the environment variable `PUML_SERVER`, e.g:
+
+```bash
+docker run -d -p 8080:8080 plantuml/plantuml-server:jetty
+export PUML_SERVER=http://localhost:8080
+gitbook serve
+```
--- test/index.js
+++ test/index.js
@@ -3,16 +3,42 @@ var tester = require('gitbook-tester');
 var assert = require('assert');
 
 describe('PlantUML', function() {
-    it('should correctly replace by img html tag', function() {
-        return tester.builder()
-            .withContent('This is a diagram:\n\n{% plantuml %}\nBob->Alice : hello\n{% endplantuml %}')
-            .withBookJson({
-                plugins: ['puml']
-            })
-            .withLocalPlugin(path.join(__dirname, '..'))
-            .create()
-            .then(function(result) {
-                assert.equal(result[0].content, '<p>This is a diagram:</p>\n<p><img src="http://www.plantuml.com/plantuml/svg/UDhZSifFqhLppCbCJbMmKiX8pSd9vm80FC85_m00"></p>')
-            });
-    });
+  it('should correctly replace by img html tag', function() {
+    return tester
+      .builder()
+      .withContent(
+        'This is a diagram:\n\n{% plantuml %}\nBob->Alice : hello\n{% endplantuml %}'
+      )
+      .withBookJson({
+        plugins: ['puml']
+      })
+      .withLocalPlugin(path.join(__dirname, '..'))
+      .create()
+      .then(function(result) {
+        assert.equal(
+          result[0].content,
+          '<p>This is a diagram:</p>\n<p><img src="http://www.plantuml.com/plantuml/svg/UDhZSifFqhLppCbCJbMmKiX8pSd9vm80FC85_m00"></p>'
+        );
+      });
+  });
+
+  it('should use the PUML_SERVER server environment variable', function() {
+    process.env.PUML_SERVER = 'MY_PUML_SERVER_URL';
+    return tester
+      .builder()
+      .withContent(
+        'This is a diagram:\n\n{% plantuml %}\nBob->Alice : hello\n{% endplantuml %}'
+      )
+      .withBookJson({
+        plugins: ['puml']
+      })
+      .withLocalPlugin(path.join(__dirname, '..'))
+      .create()
+      .then(function(result) {
+        assert.equal(
+          result[0].content,
+          '<p>This is a diagram:</p>\n<p><img src="MY_PUML_SERVER_URL/plantuml/svg/UDhZSifFqhLppCbCJbMmKiX8pSd9vm80FC85_m00"></p>'
+        );
+      });
+  });
 });
